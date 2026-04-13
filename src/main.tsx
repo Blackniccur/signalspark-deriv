60a3fa7b53df8ec1d797f92049d4461acb274ea0
Blackniccur <blackbniccur@gmail.com>
2026-04-14 01:34:59 +0300

    Add src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AnalysisTool from './components/AnalysisTool';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById('root');
+if (!root) throw new Error('Root element not found');
+
+createRoot(root).render(
+    <StrictMode>
+        <AnalysisTool />
+    </StrictMode>
+);
